0da2b292d11d63032ad46ab947223363fcd8551e
Involuntar <ilya.nevolin.2004@mail.ru>
2025-11-13 21:52:17 +0500

    Настроил кнопки фильтров

--- app/src/main/java/com/example/db_androidstudio/MainActivity.java
+++ app/src/main/java/com/example/db_androidstudio/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.db_androidstudio;
 
+import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -22,6 +30,20 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase database;
     ListView listView;
     GridLayout filterButtons;
+
+    private static boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                v.animate().scaleX(0.95f).scaleY(0.95f).setDuration(100).start();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                v.animate().scaleX(1f).scaleY(1f).setDuration(100).start();
+                break;
+        }
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,8 +74,30 @@ public class MainActivity extends AppCompatActivity {
                 getAllItems();
             }
         });
+
+        GridLayout.LayoutParams extraParams = new GridLayout.LayoutParams();
+        extraParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        extraParams.height = GridLayout.LayoutParams.WRAP_CONTENT;
+        extraParams.columnSpec = GridLayout.spec(0, 1f);
+        extraParams.rowSpec = GridLayout.spec(0);
+        extraParams.setMargins(10, 10, 10, 10);
+
+        GradientDrawable shape = new GradientDrawable();
+        GradientDrawable shapeAll = new GradientDrawable();
+
+        shape.setCornerRadius(16f); // скругление
+        shapeAll.setCornerRadius(16f);
+
+        shape.setColor(Color.parseColor("#FF6200EE")); // фон
+        shapeAll.setColor(Color.parseColor("#FE0000"));
+
+        filter_button_all.setTextColor(Color.WHITE);
+        filter_button_all.setOnTouchListener(MainActivity::onTouch);
+        filter_button_all.setBackground(shapeAll);
+        filter_button_all.setLayoutParams(extraParams);
         filterButtons.addView(filter_button_all);
 
+        int buttonsCount = 1;
         while (!cursor1.isAfterLast()) {
             String category_name = cursor1.getString(1);
 
@@ -71,8 +115,8 @@ public class MainActivity extends AppCompatActivity {
                     while (!cursor3.isAfterLast()) {
                         item = new HashMap<>();
                         item.put("item_name", cursor3.getString(0));
-                        item.put("category_name", cursor3.getString(1));
-                        item.put("description", cursor3.getString(2));
+                        item.put("category_name", "Категория: " + cursor3.getString(1));
+                        item.put("description", cursor3.getString(2) != null ? "Описание:\n" + cursor3.getString(2) : "Описание:\nНет Описания");
                         item.put("price", cursor3.getString(3) + " ₽.");
                         items.add(item);
                         cursor3.moveToNext();
@@ -92,8 +136,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.width = ViewGroup.LayoutParams.WRAP_CONTENT; // равная ширина
+            params.height = GridLayout.LayoutParams.WRAP_CONTENT;
+            params.columnSpec = GridLayout.spec(buttonsCount % 3, 1f);
+            params.rowSpec = GridLayout.spec(buttonsCount / 3);
+            params.setMargins(10, 10, 10, 10);
+
+            filter_button.setTextColor(Color.WHITE);
+            filter_button.setLayoutParams(params);
+            filter_button.setBackground(shape);
+            filter_button.setOnTouchListener(MainActivity::onTouch);
+
             filterButtons.addView(filter_button);
 
+            buttonsCount += 1;
             cursor1.moveToNext();
         }
         cursor1.close();
@@ -111,8 +168,8 @@ public class MainActivity extends AppCompatActivity {
         while (!cursor2.isAfterLast()) {
             item = new HashMap<>();
             item.put("item_name", cursor2.getString(0));
-            item.put("category_name", cursor2.getString(1));
-            item.put("description", cursor2.getString(2));
+            item.put("category_name", "Категория: " + cursor2.getString(1));
+            item.put("description", cursor2.getString(2) != null ? "Описание:\n" + cursor2.getString(2) : "Описание:\nНет Описания");
             item.put("price", cursor2.getString(3) + " ₽.");
             items.add(item);
             cursor2.moveToNext();
